3d2a3908b5af4a6fb0cfcb14039e78c8e25ea61c
Slasherbane <brobert@groupesenef.com>
2020-09-23 14:14:33 +0200

    Make a basic architecture with JNDI resource and Tomcat servlet traitement

---
+++ src/main/java/servlets/rooting.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author Benjamin
+ */
+@WebServlet(name = "rooting", urlPatterns = {"/rooting"})
+public class rooting extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, NamingException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet rooting</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet rooting at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+
+Context initCtx = new InitialContext();
+Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+// Look up our data source
+DataSource ds = (DataSource)
+  envCtx.lookup("jdbc/test");
+  Connection conn = ds.getConnection(); ;
+   out.println("Valid conexion ?: )" + conn.isValid(10));
+            try
+                     {
+       
+                    
+                PreparedStatement p =  conn.prepareStatement("select * from acompte");
+                  
+                ResultSet rs =  p.executeQuery();
+                boolean t;
+              while( t  = rs.next()){
+                   
+                    out.println(rs.getString("montantAccorde"));
+              }
+                }
+            finally{
+                conn.close();
+                initCtx.close();
+            }
+            
+            
+      
+      response.setHeader("name", "Anupam Sinha");
+   //   RequestDispatcher rd = request.getRequestDispatcher("/other");
+     // rd.forward(request, response);
+    
+
+
+         // response.sendRedirect(request.getContextPath() + "/index.jsp");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NamingException | SQLException ex) {
+            Logger.getLogger(rooting.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NamingException | SQLException ex) {
+            Logger.getLogger(rooting.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
